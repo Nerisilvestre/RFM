94eb5c9c96511d6eeabdcb5ea521017a3da29f08
Neri Silvestre <nerissfilho@gmail.com>
2018-12-13 00:39:49 -0200

    RFM full

--- Jupyter/RFM/category.py
+++ Jupyter/RFM/category.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
-
-def classifier(row):
-    if rfm2['r_cat'].between(4,5) & rfm2['fm_cat'].between(4,5):
-        return 'Champion'
-    else:
-        return 'nada'
+def cat(row):
     
-
+ if ((row['r_cat'] >= 4) & (row['r_cat'] <= 5) & (row['fm_cat'] >= 4) & (row['fm_cat'] <= 5)):
+    return'Champion'
+ elif ((row['r_cat'] >= 2) & (row['r_cat'] <= 5) & (row['fm_cat'] >= 3) & (row['fm_cat'] <= 5)):
+    return'Loyal Customers'
+ elif ((row['r_cat'] >= 3) & (row['r_cat'] <= 5) & (row['fm_cat'] >= 1) & (row['fm_cat'] <= 3)):
+    return'Potential Loyalist'
+ elif ((row['r_cat'] >= 4) & (row['r_cat'] <= 5) & (row['fm_cat'] >= 0) & (row['fm_cat'] <= 1)):
+    return'New Customers'
+ elif ((row['r_cat'] >= 3) & (row['r_cat'] <= 4) & (row['fm_cat'] >= 0) & (row['fm_cat'] <= 1)):
+    return'Promising'
+ elif ((row['r_cat'] >= 2) & (row['r_cat'] <= 3) & (row['fm_cat'] >= 2) & (row['fm_cat'] <= 3)):
+    return'Customers Needing Attention'
+ elif ((row['r_cat'] >= 2) & (row['r_cat'] <= 3) & (row['fm_cat'] >= 0) & (row['fm_cat'] <= 2)):
+    return'About to Sleep'
+ elif ((row['r_cat'] >= 0) & (row['r_cat'] <= 2) & (row['fm_cat'] >= 2) & (row['fm_cat'] <= 5)):
+    return'At Risk'
+ elif ((row['r_cat'] >= 0) & (row['r_cat'] <= 1) & (row['fm_cat'] >= 4) & (row['fm_cat'] <= 5)):
+    return"Can't Lose Them"
+ elif ((row['r_cat'] >= 1) & (row['r_cat'] <= 2) & (row['fm_cat'] >= 1) & (row['fm_cat'] <= 2)):
+    return'Hibernating'
+ elif ((row['r_cat'] >= 0) & (row['r_cat'] <= 2) & (row['fm_cat'] >= 0) & (row['fm_cat'] <= 2)):
+    return'Lost'
